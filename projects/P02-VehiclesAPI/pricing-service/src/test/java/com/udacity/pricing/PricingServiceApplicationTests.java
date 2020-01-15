c54e245cf43b41f5a9fe9a554512bbb11c4effa5
davinci <sarmileo@yahoo.com>
2020-01-15 00:07:23 -0500

    add tests to PricingServiceApplicationTests.java

--- projects/P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ projects/P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -1,16 +1,49 @@
 package com.udacity.pricing;
 
+import com.udacity.pricing.service.PricingService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class PricingServiceApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	PricingService pricingService;
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void getPriceTest() throws Exception
+	{
+		mockMvc.perform(
+				get("/services/price/").param("vehicleId", "1"))
+				.andExpect(status().isOk());
+
+	}
+
+	@Test
+	public void getPriceTestFailWithNoId() throws Exception
+	{
+		mockMvc.perform(
+				get("/services/price/"))
+				.andExpect(status().isBadRequest());
+
 	}
 
 }
