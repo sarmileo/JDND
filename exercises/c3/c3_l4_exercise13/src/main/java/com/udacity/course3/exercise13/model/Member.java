5cc88ac601c4332df05a6230d06e5c29bd0ee220
davinci <sarmileo@yahoo.com>
2020-03-24 01:11:17 -0400

    add test for the mongo repository

--- exercises/c3/c3_l4_exercise13/src/main/java/com/udacity/course3/exercise13/model/Member.java
+++ exercises/c3/c3_l4_exercise13/src/main/java/com/udacity/course3/exercise13/model/Member.java
@@ -1,12 +1,181 @@
 package com.udacity.course3.exercise13.model;
 
-import org.bson.types.ObjectId;
+import com.fasterxml.jackson.annotation.*;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "members")
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+
+@Document("club_members")
 public class Member {
 
     @Id
-    private ObjectId id;
+    private String id;
+    @JsonProperty("first_name")
+    private String firstName;
+    @JsonProperty("last_name")
+    private String lastName;
+    @JsonProperty("age")
+    private Integer age;
+    @JsonProperty("gender")
+    private String gender;
+    @JsonProperty("interests")
+    private List<String> interests = new ArrayList<String>();
+    @JsonProperty("balance")
+    private Double balance;
+    @JsonProperty("address")
+    private Address address;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    /**
+     * No args constructor for use in serialization
+     *
+     */
+    public Member() {
+    }
+
+    /**
+     *
+     * @param firstName
+     * @param lastName
+     * @param address
+     * @param gender
+     * @param balance
+     * @param id
+     * @param interests
+     * @param age
+     */
+    public Member(String id, String firstName, String lastName, Integer age, String gender, List<String> interests, Double balance, Address address) {
+        super();
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.gender = gender;
+        this.interests = interests;
+        this.balance = balance;
+        this.address = address;
+    }
+
+    @JsonProperty("_id")
+    public String getId() {
+        return id;
+    }
+
+    @JsonProperty("_id")
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @JsonProperty("first_name")
+    public String getFirstName() {
+        return firstName;
+    }
+
+    @JsonProperty("first_name")
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @JsonProperty("last_name")
+    public String getLastName() {
+        return lastName;
+    }
+
+    @JsonProperty("last_name")
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @JsonProperty("age")
+    public Integer getAge() {
+        return age;
+    }
+
+    @JsonProperty("age")
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @JsonProperty("gender")
+    public String getGender() {
+        return gender;
+    }
+
+    @JsonProperty("gender")
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @JsonProperty("interests")
+    public List<String> getInterests() {
+        return interests;
+    }
+
+    @JsonProperty("interests")
+    public void setInterests(List<String> interests) {
+        this.interests = interests;
+    }
+
+    @JsonProperty("balance")
+    public Double getBalance() {
+        return balance;
+    }
+
+    @JsonProperty("balance")
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    @JsonProperty("address")
+    public Address getAddress() {
+        return address;
+    }
+
+    @JsonProperty("address")
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).append("id", id).append("firstName", firstName).append("lastName", lastName).append("age", age).append("gender", gender).append("interests", interests).append("balance", balance).append("address", address).append("additionalProperties", additionalProperties).toString();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(firstName).append(lastName).append(address).append(gender).append(balance).append(id).append(additionalProperties).append(interests).append(age).toHashCode();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if ((other instanceof Member) == false) {
+            return false;
+        }
+        Member rhs = ((Member) other);
+        return new EqualsBuilder().append(firstName, rhs.firstName).append(lastName, rhs.lastName).append(address, rhs.address).append(gender, rhs.gender).append(balance, rhs.balance).append(id, rhs.id).append(additionalProperties, rhs.additionalProperties).append(interests, rhs.interests).append(age, rhs.age).isEquals();
+    }
+
 }
--- exercises/c3/c3_l4_exercise13/src/test/java/com/udacity/course3/exercise13/repository/MemberRepositoryTest.java
+++ exercises/c3/c3_l4_exercise13/src/test/java/com/udacity/course3/exercise13/repository/MemberRepositoryTest.java
@@ -1,15 +1,60 @@
 package com.udacity.course3.exercise13.repository;
 
+import com.udacity.course3.exercise13.model.Address;
+import com.udacity.course3.exercise13.model.Member;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.Arrays;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+import static org.springframework.data.mongodb.core.query.Query.query;
 
 @DataMongoTest
+@RunWith(SpringJUnit4ClassRunner.class)
 @EnableAutoConfiguration
 public class MemberRepositoryTest {
+    @Autowired
+    private MemberRepository memberRepository;
+    @Autowired
+    private MongoOperations mongoOperations;
 
     @Test
     public void contextLoads() {
+    }
+
+    @Test
+    public void shouldSaveADocument()
+    {
+        Member member = new Member();
+
+        // create a Member record
+        member.setFirstName("Michael");
+        member.setLastName("Jourdan");
+        member.setAge(35);
+        member.setGender("male");
+        member.setBalance(500.0);
+        member.setInterests(Arrays.asList("baseball"));
+        member.setAddress(new Address(
+                "24 Oak Breeze",
+                "Miami",
+                "Florida"
+        ));
+
+        // save the Member record
+        System.err.println("Saving : " + member.getFirstName() + " " + member.getLastName());
+        memberRepository.save(member);
+
+        Member testMember = mongoOperations.findOne(query(where("firstName").is("Michael")), Member.class);
+        assertThat(testMember.getLastName(), is(equalTo("Jourdan")));
     }
 
 
